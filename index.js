a1b47a8498f93bb2188b7ea38c15f0a0f9782233
AnonHexo <jacky.trave@icloud.com>
2022-05-02 22:17:03 +0200

    Moved token from config file to envoirment variable

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {
     encrypt,
     decrypt
 } = require('./core/crypt-utils')
-const telegram = new Telegraf(config.token)
+const telegram = new Telegraf(process.env.TOKEN)
 telegram.catch((err) => {
     console.error(err)
     config.admins.forEach(id => {
